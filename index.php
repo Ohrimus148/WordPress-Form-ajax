d7870260eeeafad5c44968e539a7279112886601
Ohrimus148 <Ohrimus@yandex.ru>
2017-07-20 12:47:46 +0300

    Added a main file Wordpress

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+    <meta charset="utf-8">
+
+    <title>Test</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/css2/normalize.css">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/css2/style.css">
+	<script src="<?php bloginfo("template_directory"); ?>/js/api.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+<body>
+
+    <section class="s_top">
+        <div class="container clearfix">
+            <div class="left">
+                <img src="<?php bloginfo("template_directory"); ?>/img2/logo.png" alt="TiniU" class="logo">
+                <div class="title">
+                    <div class="title_head">
+                        Free Christmas Printables
+                    </div>
+                    <div class="title_txt">
+                        A fun way to keep your kids busy  and have <span class="bold_txt">a Merry Christmas!</span>
+                    </div>
+                </div>
+            </div>
+            <div class="right">
+                <div class="form_wrap clearfix">
+                    <div class="form">
+                        <div class="form_head">Join our<br/> TinyU Games Club</div>
+                        <input id="username" type="text" placeholder="Your Name">
+                        <input id="email" type="text" placeholder="Your Email">
+                        <a href="#" class="form_btn" onclick = "process()"><span class="big">Download </span><br /> Christmas printables</a>
+                    </div>
+                </div>
+            </div>
+    </section>
+    <section class="s_middle">
+        <div class="container">
+            <div class="present">
+                <h1>A few reasons to get our<br/><span class="big"> awesome Christmas printables</span></h1>
+            </div>
+        </div>
+    </section>
+    <section class="s_bottom">
+        <div class="container clearfix">
+            <div class="s_bottom_head">What’s in it for you and your kids?</div>
+            <div class="circle">
+                <div class="circle_item">
+                    <img src="<?php bloginfo("template_directory"); ?>/img2/1_item.png" alt="" class="circle_icon">
+                    <div class="circle_head">Fun</div>
+                    <div class="circle_content">Your child will have fun with our festive illustrations and interesting tasks.</div>
+                </div>
+                <div class="circle_item">
+                    <img src="<?php bloginfo("template_directory"); ?>/img/2_item.png" alt="" class="circle_icon">
+                    <div class="circle_head">Education</div>
+                    <div class="circle_content">Our printables help develop logic, train memory and improve fine motor skills.</div>
+                </div>
+                <div class="circle_item">
+                    <img src="<?php bloginfo("template_directory"); ?>/img2/3_item.png" alt="" class="circle_icon">
+                    <div class="circle_head">Season Spirit</div>
+                    <div class="circle_content">Help your tiny discover more about Christmas, its attributes and traditions.</div>
+                </div>
+                <div class="circle_item">
+                    <img src="<?php bloginfo("template_directory"); ?>/img2/4_item.png" alt="" class="circle_icon">
+                    <div class="circle_head">Time for yourself</div>
+                    <div class="circle_content">We’ve done everything to give you a few free minutes.</div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
